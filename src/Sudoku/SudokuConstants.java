4fda9770963c85fd93e2c5ef22fdecd4a3d63c7c
yeshaa23 <ayeshanazkiya@gmail.com>
2024-12-21 18:38:04 +0700

    masukin nama kelompok

--- src/Sudoku/SudokuConstants.java
+++ src/Sudoku/SudokuConstants.java
@@ -1,3 +1,12 @@
+/**
+ * ES234317-Algorithm and Data Structures
+ * Semester Ganjil, 2024/2025
+ * Group Capstone Project
+ * Group #16
+ * 1 - 5026231038 - Nabila Shinta Luthfia
+ * 2 - 5026231125 - Ayesha Hana Azkiya
+ * 3 - 5026231139 - Amandea Chandiki Larasati
+ */
 package Sudoku;
 /**
  * Define the named constants used in many classes.
